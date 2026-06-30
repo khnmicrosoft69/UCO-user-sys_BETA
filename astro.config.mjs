360d4bcd84f21b22e9fdec85b42c5d45848383a1
Khan <ferzkhan668@gmail.com>
2026-06-30 09:45:26 +0800

    2nd commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,18 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 
-import node from "@astrojs/node";
-
+// https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: vercel(),
   integrations: [react()],
-
   vite: {
     plugins: [tailwindcss()],
+    optimizeDeps: {
+      // Forces Vite to correctly look up the ESM exports for React 19
+      include: ['react', 'react-dom', 'react-dom/client'], 
+    },
   },
-
-  adapter: node({
-    mode: "standalone",
-  }),
 });
